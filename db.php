89772be6b2e25965c3ad28e139c40a37af0e69a1
Ponomarenko Daria <52483452+Ponomarenko-Daria@users.noreply.github.com>
2022-07-21 16:10:11 +0300

    Update db.php

--- db.php
+++ db.php
@@ -3,7 +3,7 @@ session_start();
 
 $conn = mysqli_connect(
   getenv("MYSQL_SERVER"),
-  getenv("MYSQL_USER"),
+  getenv("MYSQL_ROOT"),
   getenv("MYSQL_ROOT_PASSWORD"),
   getenv("MYSQL_DATABASE"),
 ) or die(mysqli_error($mysqli));
